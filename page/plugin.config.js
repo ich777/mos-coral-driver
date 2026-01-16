8d1dab902fc4e0ffa5bcb07439324e9d5d3a20ed
Christoph <christophhummer@gmail.com>
2026-01-16 13:44:42 +0100

    Add files via upload

---
+++ page/plugin.config.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'coral-driver',
+  displayName: 'Google Coral Driver',
+  description: 'Google Coral Driver for MOS',
+  version: '0.0.0',
+  icon: 'https://raw.githubusercontent.com/ich777/docker-templates/master/ich777/images/coral-driver.png',
+  author: 'ich777',
+  homepage: 'https://github.com/google/gasket-driver',
+};
